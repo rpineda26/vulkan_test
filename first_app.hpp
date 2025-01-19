93fca2ab1af705e7a0d74810c6638e0335b39d1b
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2025-01-19 23:49:04 +0800

    rollback

--- first_app.hpp
+++ first_app.hpp
@@ -1,32 +1,16 @@
 #pragma once
 #include "ve_window.hpp"
-#include "ve_pipeline.hpp"
-#include "ve_device.hpp"
-#include "ve_swap_chain.hpp"
 
-#include <memory>
 #include <vector>
 namespace ve {
     class FirstApp{
         public:
             static constexpr int WIDTH = 800;
             static constexpr int HEIGHT = 600;
-            FirstApp();
-            ~FirstApp();
-            FirstApp(const FirstApp&) = delete;
-            FirstApp& operator=(const FirstApp&) = delete;
             void run();
              
         private:
-            void createPipelineLayout();
-            void createPipeline();
-            void createCommandBuffers();
-            void drawFrame();
             VeWindow veWindow{WIDTH, HEIGHT, "First App"};
-            VeDevice veDevice{veWindow};
-            VeSwapChain veSwapChain{veDevice, veWindow.getExtent()};
-            std::unique_ptr<VePipeline> vePipeline;
-            VkPipelineLayout pipelineLayout;
-            std::vector<VkCommandBuffer> commandBuffers;
+
     };
 }
--- ve_window.hpp
+++ ve_window.hpp
@@ -1,22 +1,21 @@
-//Vulkan engine window header file
 #pragma once
 #define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h> 
+#include <GLFW/glfw3.h>
 #include <string>
+
 namespace ve {
     class VeWindow {
         public:
             VeWindow(int w, int h, std::string name);
             ~VeWindow();
-            VeWindow(const VeWindow &) = delete; 
-            VeWindow &operator=(const VeWindow &) = delete; 
+            VeWindow(const VeWindow&) = delete;
+            VeWindow& operator=(const VeWindow&) = delete;
             bool shouldClose() { return glfwWindowShouldClose(window); }
-            VkExtent2D getExtent() { return {static_cast<uint32_t>(width), static_cast<uint32_t>(height)}; }
-            void createWindowSurface(VkInstance instance, VkSurfaceKHR *surface); 
         private:
             void initWindow();
+            const int WIDTH;
+            const int HEIGHT;
             std::string windowName;
-            GLFWwindow *window;
-            const int width, height;
+            GLFWwindow* window;
     };
 }
